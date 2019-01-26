c1acebd725d32c3f210d14cb9eb2473663a0f554
Miuta Mihai <miuta.mihai@gmail.com>
2019-01-26 19:32:59 +0200

    a

---
+++ CalManV1/App_Data/Resources/Produce.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resources
+{
+    public class Produce
+    {
+        public string Name { get; set; }
+        public double Cal100 { get; set; }
+        public double Cal50 { get; set; }
+        public double Cal10 { get; set; }
+    }
+}
---
+++ CalManV1/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Logic;
+
+namespace CalManV1
+{
+    public partial class _Default : Page
+    {
+        Calculate logic = new Calculate();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            logic.SetInput(System.Convert.ToDouble(TextBox1.Text));
+            Label1.Text = logic.Result();
+        }
+    }
+}
